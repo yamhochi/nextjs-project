abf6beec341b2f3cebe87a441f94ec181f434e28
yamhochi <yam.hoechee@gmail.com>
2022-06-23 09:17:29 +1000

    first update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const { withKeystone } = require("@keystone-6/core/next");
+module.exports = withKeystone({
+  reactStrictMode: true,
+ });
